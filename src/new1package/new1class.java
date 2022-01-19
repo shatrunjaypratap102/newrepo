0d1d51d741c67f452c5b2dd32e158717eb48d785
vipul <vipul.gupta@zestmoney.in>
2022-01-19 13:58:23 +0530

    first commit

---
+++ src/new1package/new1class.java
@@ -0,0 +1,10 @@
+package new1package;
+
+public class new1class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hey i hav created the new java proejct");
+	}
+
+}
